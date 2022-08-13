60c05cb010f36dabfeb94797afb5188cbe03ec9b
Valera <valerakaktotak@gmail.com>
2022-08-13 17:11:41 +0300

    first simple slider

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+// ф-ция ввода значений и операций в блок табло с помощью кнопок
+var buttonNumbers = document.querySelectorAll('.calc table tr td button');
+function numberAction (event){
+
+    var currentItem = event.currentTarget;
+    var value = currentItem.innerHTML;
+    var data = document.querySelectorAll('.table .result');
+    if(data[0].innerHTML === '0' && value === '.'){
+        data[0].innerHTML += value;
+    }
+    else if(data[0].innerHTML === '0'){
+        data[0].innerHTML = value;
+    }
+    else{
+        data[0].innerHTML += value;
+    }
+}
+buttonNumbers.forEach(element => element.addEventListener('click', numberAction))
+
+// ф-ция очистки данных в табло
+var clearButton = document.getElementById('clear')
+function clearAction(){
+    var data = document.querySelectorAll('.table .result');
+    data[0].innerHTML = '0';
+}
+clearButton.addEventListener('click', clearAction);
+
+//функция подсчета выражения с табло (=)
+var score = document.getElementById('score');
+score.addEventListener('click', result)
+function result(){
+    var stringResult = document.querySelectorAll('.table .result');
+    var result = eval(stringResult[0].innerHTML);
+    stringResult[0].innerHTML = result;
+    // finalResult[0].innerHTML = result;
+}
+
+
--- slider/js/script.js
+++ slider/js/script.js
@@ -1,39 +1,31 @@
+var sliderImages = document.querySelectorAll('.my_slider .slider_image');
+var sliderPrevBtn = document.getElementById('prev_btn');
+var sliderNextBtn = document.getElementById('next_btn');
+var currentIndex = 0;
 
-// ф-ция ввода значений и операций в блок табло с помощью кнопок
-var buttonNumbers = document.querySelectorAll('.calc table tr td button');
-function numberAction (event){
+function actionPrev() {
+  var currentItem = sliderImages[currentIndex];
+  currentItem.classList.remove('active');
+  if (currentIndex === 0){
+    currentIndex = sliderImages.length - 1;
+  } else {
+    currentIndex--;
+  }
+  currentItem = sliderImages[currentIndex];
+  currentItem.classList.add('active');
 
-    var currentItem = event.currentTarget;
-    var value = currentItem.innerHTML;
-    var data = document.querySelectorAll('.table .result');
-    if(data[0].innerHTML === '0' && value === '.'){
-        data[0].innerHTML += value;
-    }
-    else if(data[0].innerHTML === '0'){
-        data[0].innerHTML = value;
-    }
-    else{
-        data[0].innerHTML += value;
-    }
 }
-buttonNumbers.forEach(element => element.addEventListener('click', numberAction))
-
-// ф-ция очистки данных в табло
-var clearButton = document.getElementById('clear')
-function clearAction(){
-    var data = document.querySelectorAll('.table .result');
-    data[0].innerHTML = '0';
-}
-clearButton.addEventListener('click', clearAction);
-
-//функция подсчета выражения с табло (=)
-var score = document.getElementById('score');
-score.addEventListener('click', result)
-function result(){
-    var stringResult = document.querySelectorAll('.table .result');
-    var result = eval(stringResult[0].innerHTML);
-    stringResult[0].innerHTML = result;
-    // finalResult[0].innerHTML = result;
+function actionNext() {
+  var currentItem = sliderImages[currentIndex];
+  currentItem.classList.remove('active');
+  if (currentIndex === sliderImages.length-1){
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  currentItem = sliderImages[currentIndex];
+  currentItem.classList.add('active');
 }
 
-
+sliderPrevBtn.addEventListener('click', actionPrev);
+sliderNextBtn.addEventListener('click', actionNext);
