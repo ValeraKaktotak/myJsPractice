0797c9aa6eb4b65df0787efd88ed67187919ae15
Valera <valerakaktotak@gmail.com>
2022-08-15 22:27:56 +0300

    change slider structure to object structure

--- slider/js/script.js
+++ slider/js/script.js
@@ -4,27 +4,28 @@ var sliderNextBtn = document.getElementById('next_btn');
 var currentIndex = 0;
 
 function actionPrev() {
-  var currentItem = sliderImages[currentIndex];
-  currentItem.classList.remove('active');
-  if (currentIndex === 0){
-    currentIndex = sliderImages.length - 1;
-  } else {
-    currentIndex--;
-  }
-  currentItem = sliderImages[currentIndex];
-  currentItem.classList.add('active');
+    var currentItem = sliderImages[currentIndex];
+    currentItem.classList.remove('active');
+    if (currentIndex === 0) {
+        currentIndex = sliderImages.length - 1;
+    } else {
+        currentIndex--;
+    }
+    currentItem = sliderImages[currentIndex];
+    currentItem.classList.add('active');
 
 }
+
 function actionNext() {
-  var currentItem = sliderImages[currentIndex];
-  currentItem.classList.remove('active');
-  if (currentIndex === sliderImages.length-1){
-    currentIndex = 0;
-  } else {
-    currentIndex++;
-  }
-  currentItem = sliderImages[currentIndex];
-  currentItem.classList.add('active');
+    var currentItem = sliderImages[currentIndex];
+    currentItem.classList.remove('active');
+    if (currentIndex === sliderImages.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    currentItem = sliderImages[currentIndex];
+    currentItem.classList.add('active');
 }
 
 sliderPrevBtn.addEventListener('click', actionPrev);
