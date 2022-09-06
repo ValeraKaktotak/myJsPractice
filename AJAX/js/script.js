c259e440e350d3d9bbb4d4a2cb94ab1da1966501
Valera <valerakaktotak@gmail.com>
2022-09-06 11:22:05 +0300

    api GET POST PUT DELETE

--- AJAX/js/script.js
+++ AJAX/js/script.js
@@ -1,17 +1,27 @@
 let clickButton = document.querySelector('#click_me');
+let clickPostButton = document.querySelector('#click_me_post');
 let pageNumber = document.querySelector('#page_number');
 let pageLimit = document.querySelector('#page_limit');
 let queryInfo = document.querySelector('.query_info');
-let apiUrl = 'https://retoolapi.dev/2XHDRY/data';
+let apiUrl = 'https://api-generator.retool.com/47ITxJ/data';
+let apiUrlPost = 'https://api-generator.retool.com/47ITxJ/data';
 
 clickButton.addEventListener('click', ()=>{
-    let promise = makeRequest(pageNumber.value, pageLimit.value, apiUrl);
+    let promise = makeGetRequest(pageNumber.value, pageLimit.value, apiUrl);
     promise.then(data => {
+        queryInfo.innerHTML = '';
         data.forEach(element=>{
             let myDiv = document.createElement('div');
             myDiv.textContent = element.Column1;
             queryInfo.append(myDiv);
         });
+    });
+});
+
+clickPostButton.addEventListener('click', ()=>{
+    let promise = makePostRequest(apiUrlPost);
+    promise.then(data => {
+        alert(`Ваша запись ${data.id} добавлена`);
     });
 });
 
